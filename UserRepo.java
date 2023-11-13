bb1eb4090479b1e2f0ac6fd74ed6b3a3cab40ae2
suvam sarangi <138117866+SuvamSarangi@users.noreply.github.com>
2023-11-13 11:22:41 +0530

    Add files via upload

---
+++ UserRepo.java
@@ -0,0 +1,13 @@
+package com.example.placementcell.Repository;
+
+import com.example.placementcell.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+        Optional<User> findByEmail(String email);
+}
